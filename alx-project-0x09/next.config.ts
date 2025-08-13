7c80d64cc329e267606ee0fe140da7f46ce68f6a
SylvesterK-code <sylvesterkeystarling@gmail.com>
2025-08-13 22:18:53 +0000

    Generate Image based on user prompt

--- alx-project-0x09/next.config.ts
+++ alx-project-0x09/next.config.ts
@@ -1,8 +1,26 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   reactStrictMode: true,
+// };
+
+// export default nextConfig;
+
+
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'prlabsapi.com',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
